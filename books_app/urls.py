ba6e8237c130d372abd5ec7c1bd7e5046a054374
njo726 <139063593+njo726@users.noreply.github.com>
2024-03-24 22:42:20 +0000

    display books dynamically and add filter by  genere

--- books_app/urls.py
+++ books_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.demo, name='demo'),
+    path('', views.listBooks, name='listBooks'),
 ]
--- books_app/views.py
+++ books_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-def demo(request):
-    return render(request, 'pages/books.html')
+
+
+def __getBooks():
+    return [
+        {'id': 1, 'title': 'The Great Gatsby', 'author': 'F. Scott Fitzgerald', 'genre': 'Classic', 'status':'متاح', 'price': 15, 'image_url': 'https://example.com/images/gatsby.jpg'},
+        {'id': 2, 'title': '1984', 'author': 'George Orwell', 'genre': 'Dystopian','status':'متاح', 'price': 12, 'image_url': 'https://example.com/images/1984.jpg'},
+        {'id': 3, 'title': 'To Kill a Mockingbird', 'author': 'Harper Lee', 'genre': 'Classic', 'status':'متاح', 'price': 18, 'image_url': 'https://example.com/images/mockingbird.jpg'},
+        {'id': 4, 'title': 'The Catcher in the Rye', 'author': 'J.D. Salinger', 'genre': 'Classic', 'status':'متاح', 'price': 10, 'image_url': 'https://example.com/images/catcher.jpg'},
+        {'id': 5, 'title': 'The Hobbit', 'author': 'J.R.R. Tolkien', 'genre': 'Fantasy', 'status':'متاح', 'price': 20, 'image_url': 'https://example.com/images/hobbit.jpg'},
+        {'id': 6, 'title': 'Pride and Prejudice', 'author': 'Jane Austen', 'genre': 'Romance', 'status':'متاح', 'price': 14, 'image_url': 'https://example.com/images/pride.jpg'}
+    ]
+def listBooks(request):
+    books = __getBooks()
+    return render(request, 'pages/books.html', {'books': books})
