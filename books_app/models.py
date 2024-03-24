46c37ac374960bc2139acfdbedb455c60058dacf
njo726 <139063593+njo726@users.noreply.github.com>
2024-03-24 22:49:22 +0000

    create model and migrate and create migrations

--- books_app/models.py
+++ books_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Book(models.Model):
+    title = models.TextField()
+    author = models.TextField()
+    genre = models.TextField()
+    status = models.TextField()  # For example, 'متاح', 'تم استعارته', تم بيعه.
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
